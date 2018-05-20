a1d21ceda98faf2fd740eed38907f07526e5e79c
Ao Liu <frankliuao@gmail.com>
2018-05-20 15:53:36 -0500

    script update

--- cmake_make.sh
+++ cmake_make.sh
@@ -2,21 +2,26 @@
 
 if [ $# -ge 1 ]
 then
-  if [ $1="clean" ] 
+  if [ $1 = "clean" ] 
   then
+    echo "Cleaning cmake residuals and logs"
     if [ -e ./build/Makefile ]
     then
       cd ./build/; make clean; cd - 1>/dev/null
     fi
     rm -rf build/
+    exit 0
+  elif [ $1 = "make" ] 
+  then
+    :
   else
     echo "Command not found"
     exit 1
   fi
-else
-  if [ ! -d ./build/ ] 
-  then
-    mkdir ./build/
-  fi
-  cd ./build/; cmake ..; make; cd - 1>/dev/null
 fi
+echo "Doing a regular cmake setup for making"
+if [ ! -d ./build/ ] 
+then
+  mkdir ./build/
+fi
+cd ./build/; cmake ..; make; cd - 1>/dev/null
